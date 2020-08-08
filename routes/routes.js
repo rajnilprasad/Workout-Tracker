39d576e2e0f9f0a5a41001db308fbcb1c9cbe484
Rajnil Prasad <rajnilprasad.rp@gmail.com>
2020-08-08 11:38:36 -0700

    mybaby

--- routes/routes.js
+++ routes/routes.js
@@ -1,22 +1,16 @@
 const path = require('path')
-
-module.exports = function (app) {
-
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-
-    app.get('/stats', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/stats.html'));
-    });
-
-    app.get('/exercise', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/exercise.html'));
-    });
-};
-
-
+const app = require("express").Router();
+app.get('/', (req, res) => {
+    // res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+app.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
+app.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+module.exports = app;
 // require('./routes/routes.js')(app);
-
 // HTML routes needed
+
 
